dcd42448e6d6222981a5315daa0985cd668f655c
Simona Pizio <simona.pizio@gmail.com>
2023-11-03 15:35:38 +0100

    #5 - Implement GET strips - import database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './db.sqlite');
 
 module.exports = app;
 
